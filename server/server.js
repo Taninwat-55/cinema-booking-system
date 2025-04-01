00c64ac7ef32f30bd55d6efe5d6846df17d89c40
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-04-01 11:41:18 +0200

    delete env  from client, modified .env in server with api key

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
-//const omdbKey = process.env.OMDB_API_KEY;
+const port = process.env.PORT || 3001;
+// const omdbKey = process.env.OMDB_API_KEY;
 
 app.use(express.json());
 
