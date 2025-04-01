0eb6a08f97e7c3e07014ab95e9f5b459aa212a97
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-04-01 12:43:23 +0200

    controllers, middlewares and routes have more files

--- server/seed/fetchMovies.js
+++ server/seed/fetchMovies.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
-const db = require('../db/database')
+const db = require('../db/database');
 
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
+function saveMovieToDatabase(movie) {
+  const stmt = db.prepare(`
+        INSERT OR IGNORE INTO movies (title, poster_url, runtime, description, trailer_url, omdb_id) 
+        VALUE (?, ?, ?, ?, ?, ?)
+    `);
+
+  stmt.run(
+    movie.Title,
+    movie.Poster,
+    movie.Runtime,
+    movie.Plot,
+    '', // Behöver trailer
+    movie.imdbID
+  );
+}
