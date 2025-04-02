bf3016e85d8d7da5373e049ea11d0bee5a4c52e5
Taninwat-55 <60184555+Taninwat-55@users.noreply.github.com>
2025-04-02 10:16:37 +0200

    adding test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import Text from './components/Text';
 
 export default function App() {
   return (
     <div>
       <Outlet />
+
+      <h1>Cinema Booking System</h1>
+      <Text />
     </div>
   );
 }
